aa50de8bd3eef7ccf2e1eb04ff25d3709df322b0
Andrew Montgomery <andrewm@streamelements.com>
2022-12-01 10:29:30 +0000

    day01 solved

--- src/day01.zig
+++ src/day01.zig
@@ -10,8 +10,36 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day01.txt");
 
+const Elf = struct {
+    snacks: std.ArrayList(u32),
+    total: u32,
+};
+
+pub fn compareElf(_: void, a: Elf, b: Elf) bool {
+    return a.total < b.total;
+}
+
 pub fn main() !void {
-    
+    var splits = split(u8, data, "\n");
+    var elves = List(Elf).init(gpa);
+    var elf = Elf{ .snacks = List(u32).init(gpa), .total = 0 };
+    while (splits.next()) |line| {
+        if (std.mem.eql(u8, line, "")) {
+            try elves.append(elf);
+            elf = Elf{ .snacks = List(u32).init(gpa), .total = 0  };
+            continue;
+        }
+        const calories = parseInt(u32, line, 10);
+        try elf.snacks.append(try calories);
+        elf.total += try calories;
+    }
+
+    sort(Elf, elves.items, {}, compareElf);
+    print("Part 1\n", .{});
+    print("{}\n", .{elves.items[elves.items.len-1].total});
+    print("Part 2\n", .{});
+    const total = elves.items[elves.items.len-1].total + elves.items[elves.items.len-2].total + elves.items[elves.items.len-3].total;
+    print("{}\n", .{total});
 }
 
 // Useful stdlib functions
