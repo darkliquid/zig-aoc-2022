d0393e5f8b27521ba472f1289f101df467a52199
Andrew Montgomery <andrewm@streamelements.com>
2022-12-02 07:09:07 +0000

    day02 solved

--- src/day02.zig
+++ src/day02.zig
@@ -10,8 +10,80 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day02.txt");
 
+const Move = enum(u32) {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+};
+
+const Result = enum(u32) {
+    Win = 6,
+    Draw = 3,
+    Lose = 0,
+};
+
 pub fn main() !void {
-    
+    var p1score: u32 = 0;
+    var p2score: u32 = 0;
+    var splits = split(u8, data, "\n");
+    while (splits.next()) |line| {
+        if (std.mem.eql(u8, line, "")) {
+            break;
+        }
+
+        const oMove = switch (line[0]) {
+            'A' => Move.Rock,
+            'B' => Move.Paper,
+            'C' => Move.Scissors,
+            else => unreachable,
+        };
+        const pMove = switch (line[2]) {
+            'X' => Move.Rock,
+            'Y' => Move.Paper,
+            'Z' => Move.Scissors,
+            else => unreachable,
+        };
+        const pResult = switch (line[2]) {
+            'X' => Result.Lose,
+            'Y' => Result.Draw,
+            'Z' => Result.Win,
+            else => unreachable,
+        };
+
+        p1score += @enumToInt(pMove);
+        if (oMove == pMove) {
+            p1score += @enumToInt(Result.Draw);
+        } else if (oMove == Move.Rock and pMove == Move.Scissors) {
+            p1score += @enumToInt(Result.Lose);
+        } else if (oMove == Move.Paper and pMove == Move.Rock) {
+            p1score += @enumToInt(Result.Lose);
+        } else if (oMove == Move.Scissors and pMove == Move.Paper) {
+            p1score += @enumToInt(Result.Lose);
+        } else {
+            p1score += @enumToInt(Result.Win);
+        }
+
+        p2score += @enumToInt(pResult);
+        switch (oMove) {
+            Move.Rock => switch (pResult) {
+                Result.Win => p2score += @enumToInt(Move.Paper),
+                Result.Draw => p2score += @enumToInt(Move.Rock),
+                Result.Lose => p2score += @enumToInt(Move.Scissors),
+            },
+            Move.Paper => switch (pResult) {
+                Result.Win => p2score += @enumToInt(Move.Scissors),
+                Result.Draw => p2score += @enumToInt(Move.Paper),
+                Result.Lose => p2score += @enumToInt(Move.Rock),
+            },
+            Move.Scissors => switch (pResult) {
+                Result.Win => p2score += @enumToInt(Move.Rock),
+                Result.Draw => p2score += @enumToInt(Move.Scissors),
+                Result.Lose => p2score += @enumToInt(Move.Paper),
+            },
+        }
+    }
+    print("Part 1: {d}\n", .{p1score});
+    print("Part 2: {d}\n", .{p2score});
 }
 
 // Useful stdlib functions
