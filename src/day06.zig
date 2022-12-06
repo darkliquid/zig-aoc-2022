2bf5118ccc51ad5ce1856005045095bbb937b765
Andrew Montgomery <andrewm@streamelements.com>
2022-12-06 09:33:51 +0000

    day06 solved

--- src/day06.zig
+++ src/day06.zig
@@ -11,7 +11,47 @@ const gpa = util.gpa;
 const data = @embedFile("data/day06.txt");
 
 pub fn main() !void {
-    
+    var splits = split(u8, data, "\n");
+    var l = List(u8).init(gpa);
+    const line = splits.next() orelse "";
+
+    // packet header
+    var idx: usize = 0;
+    for (line) |c, i| {
+        try l.insert(0, c);
+        if (l.items.len > 4) {
+            _ = l.pop();
+            var m = Map(u8, void).init(gpa);
+            for (l.items) |x| {
+                try m.put(x, {});
+            }
+            if (m.count() == 4) {
+                idx = i+1;
+                print("Packet marker at index: {d}\n", .{idx});
+                break;
+            }
+        }
+    }
+
+    l.clearAndFree();
+
+    // message header
+    for (line[idx..]) |c, i| {
+        try l.insert(0, c);
+        if (l.items.len > 14) {
+            _ = l.pop();
+            var m = Map(u8, void).init(gpa);
+            for (l.items) |x| {
+                try m.put(x, {});
+            }
+            if (m.count() == 14) {
+                idx += i+1;
+                print("Message marker at index: {d}\n", .{idx});
+                break;
+            }
+        }
+
+    }
 }
 
 // Useful stdlib functions
