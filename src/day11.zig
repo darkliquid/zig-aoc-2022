d34ccdbbdf0958b570c81276d14f320fbc7d1f1c
Andrew Montgomery <andrewm@streamelements.com>
2022-12-13 10:32:56 +0000

    day11 wip

--- src/day11.zig
+++ src/day11.zig
@@ -10,8 +10,191 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day11.txt");
 
+const Item = u128;
+
+const Op = enum {
+    Add,
+    Mul,
+};
+
+const Operation = struct {
+    lhs: ?Item,
+    rhs: ?Item,
+    op: Op,
+
+    pub fn format(value: @This(), comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) std.os.WriteError!void {
+        if (value.lhs) |n| {
+            try writer.print("{d} ", .{n});
+        } else {
+            try writer.print("old ", .{});
+        }
+        switch (value.op) {
+            Op.Add => try writer.print("+ ", .{}),
+            Op.Mul => try writer.print("* ", .{}),
+        }
+        if (value.rhs) |n| {
+            return writer.print("{d}", .{n});
+        } else {
+            return writer.print("old", .{});
+        }
+    }
+
+    pub fn apply(self: @This(), item: Item) Item {
+        return switch(self.op) {
+            Op.Add => return (self.lhs orelse item) + (self.rhs orelse item),
+            Op.Mul => return (self.lhs orelse item) * (self.rhs orelse item),
+        };
+    }
+};
+
+const Monkey = struct {
+    num: usize,
+    items: List(Item),
+    operation: Operation,
+    test_div: usize,
+    test_true: usize,
+    test_false: usize,
+    inspections: usize,
+
+    const Self = @This();
+
+    pub fn format(value: Self, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) std.os.WriteError!void {
+        try writer.print("Monkey {d}:\n  Items: ", .{ value.num});
+
+        var i:usize = 0;
+        while (i < value.items.items.len) : (i += 1) {
+            if (i > 0) {
+                try writer.print(", ", .{});
+            }
+            try writer.print("{d}", .{ value.items.items[i] });
+        }
+
+        try writer.print("\n  Operation: new = {s}", .{value.operation});
+        try writer.print("\n  Test: divisible by {d}", .{value.test_div});
+        try writer.print("\n    If true: throw to monkey {d}", .{value.test_true});
+        return writer.print("\n    If false: throw to monkey {d}\n", .{value.test_false});
+    }
+
+    pub fn throw(self: *Self, monkeys: *List(Monkey)) !void {
+        if (self.items.items.len == 0) {
+            return;
+        }
+
+        for (self.items.items) |item| {
+            self.inspections += 1;
+
+            var new_item = self.operation.apply(item);
+            new_item = @divFloor(new_item, 3);
+            if (@rem(item, self.test_div) == 0) {
+                try monkeys.items[self.test_true].items.append(new_item);
+            } else {
+                try monkeys.items[self.test_false].items.append(new_item);
+            }
+        }
+        self.items.clearAndFree();
+    }
+};
+
 pub fn main() !void {
-    
+    var splits = split(u8, data, "\n");
+    var monkeys = List(Monkey).init(gpa);
+    defer monkeys.deinit();
+
+
+    var cur_monkey: ?*Monkey = null;
+    while (splits.next()) |line| {
+        if (eql(u8, line, "")) {
+            continue;
+        }
+
+        if (startsWith(u8, line, "Monkey ")) {
+            const monkey_num = line[7]-'0';
+            try monkeys.append(Monkey{
+                .num = monkey_num,
+                .items = List(Item).init(gpa),
+                .operation = Operation{
+                    .lhs = undefined,
+                    .rhs = undefined,
+                    .op = undefined,
+                },
+                .test_div = undefined,
+                .test_true = undefined,
+                .test_false = undefined,
+                .inspections = 0,
+            });
+            cur_monkey = &(monkeys.items[monkey_num]);
+        } else if (startsWith(u8, line, "  Starting items: ")) {
+            var items = split(u8, line[18..], ", ");
+            while (items.next()) |item| {
+                try cur_monkey.?.items.append(try parseInt(Item, item, 10));
+            }
+        } else if (startsWith(u8, line, "  Operation: new = ")) {
+            var tokens = tokenize(u8, line[19..], " ");
+            const lhs = tokens.next() orelse unreachable;
+            const op = tokens.next() orelse unreachable;
+            const rhs = tokens.next() orelse unreachable;
+
+            if (lhs[0] == 'o') {
+                cur_monkey.?.operation.lhs = null;
+            } else {
+                cur_monkey.?.operation.lhs = try parseInt(Item, lhs, 10);
+            }
+
+            cur_monkey.?.operation.op = switch(op[0]) {
+                '+' => Op.Add,
+                '*' => Op.Mul,
+                else => unreachable,
+            };
+
+            if (rhs[0] == 'o') {
+                cur_monkey.?.operation.rhs = null;
+            } else {
+                cur_monkey.?.operation.rhs = try parseInt(Item, rhs, 10);
+            }
+        } else if (startsWith(u8, line, "  Test: divisible by ")) {
+            cur_monkey.?.test_div = try parseInt(u8, line[21..], 10);
+        } else if (startsWith(u8, line, "    If true: throw to monkey ")) {
+            cur_monkey.?.test_true = line[29] - '0';
+        } else if (startsWith(u8, line, "    If false: throw to monkey ")) {
+            cur_monkey.?.test_false = line[30] - '0';
+        } else {
+            continue;
+        }
+    }
+
+    print("Inital state:\n", .{});
+    for (monkeys.items) |*monkey| {
+        print("{}\n", .{monkey});
+    }
+
+    print("\n", .{});
+
+    var round:usize = 0;
+    while(round < 20) : (round += 1) {
+        for (monkeys.items) |*monkey| {
+            try monkey.throw(&monkeys);
+        }
+
+        print("After Round {d}:\n\n", .{round+1});
+        for (monkeys.items) |*monkey| {
+            print("{}\n", .{monkey});
+        }
+
+        print("\n", .{});
+    }
+
+    sort(Monkey, monkeys.items, void{}, inspectionOrder);
+
+    for (monkeys.items) |*monkey| {
+        print(">> Monkey {d} inspected {d} times\n", .{monkey.num, monkey.inspections});
+    }
+
+    print("\nMonkey business: {d}", .{monkeys.items[0].inspections * monkeys.items[1].inspections});
+}
+
+fn inspectionOrder(ctx: void, lhs: Monkey, rhs: Monkey) bool {
+    _ = ctx;
+    return rhs.inspections < lhs.inspections;
 }
 
 // Useful stdlib functions
@@ -26,6 +209,8 @@ const lastIndexOfStr = std.mem.lastIndexOfLinear;
 const trim = std.mem.trim;
 const sliceMin = std.mem.min;
 const sliceMax = std.mem.max;
+const eql = std.mem.eql;
+const startsWith = std.mem.startsWith;
 
 const parseInt = std.fmt.parseInt;
 const parseFloat = std.fmt.parseFloat;
